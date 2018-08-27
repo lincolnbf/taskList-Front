c1b215aed31c3e8d43b10b5a94465ab5ac3c0852
lincolnbf <lincolnbf@gmail.com>
2018-08-27 14:30:45 -0300

    Refeito a parte de inserção, salvando já no Banco de dados, e atualizando automaticamente após salvar

--- Controllers/task.js
+++ Controllers/task.js
@@ -1,7 +1,26 @@
-angular.module('App', [])
-.controller('Hello', function($scope, $http) {
-    $http.get('http://localhost:8080/tasks/').
-        then(function(response) {
-            $scope.greeting = response.data;
-        });
+angular.module("App", []).controller("Hello", function($scope, $http) {
+  $http.get("http://localhost:8080/tasks/").then(function(response) {
+    $scope.tasks = response.data;
+  });
+
+  function genericSuccess(res) {
+    return res.data.data; 
+  }
+
+  $scope.add = function(tasks) {
+    return $http
+      .post("http://localhost:8080/tasks", tasks)
+      .then(function(success) {
+        load();
+        return genericSuccess(success);
+        
+      });
+  };
+
+  var load = function() {
+    $http.get("http://localhost:8080/tasks/").then(function(response) {
+      $scope.tasks = response.data;
+    });
+  };
+
 });
